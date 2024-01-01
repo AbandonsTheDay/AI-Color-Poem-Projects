396d88424b1e380626c654c925f2676bba2dbee3
SighCodes <deardarkheartfantasy@gmail.com>
2024-01-01 18:29:41 -0500

    HTML, JS
    Removed dummy poem. Fine tuned prompt and context in JS.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function poemResult(response) {
   new Typewriter("#poem", {
-    strings: "Roses are red...",
+    strings: response.data.answer,
     autoStart: true,
     cursor: "",
     delay: 10,
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let userSubmission = document.querySelector("#submission");
+  let prompt = `write a nice short color poem about ${userSubmission.value}`;
+  let context =
+    "You are a savy poet, and your expertise is writing poems about color. Use basic html on the poem text when generating the poem, and separate each line with a <br />. Only style the text in the color black. Sign the poem as '- SheCodes AI'.";
+  let apiKey = "2f3f1baf409eboat1de63d1abc36da6b";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(poemResult);
 }
 
 let formElement = document.querySelector("#poem-gen-form");

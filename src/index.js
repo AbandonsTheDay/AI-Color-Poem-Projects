95f9014b18e13657ca94d27b9e4c6dbcef4bf0c0
SighCodes <deardarkheartfantasy@gmail.com>
2024-01-01 19:23:11 -0500

    CSS, JS, HTML
    Added 'hidden' class to poem div, then hid poem space in Javascript and HTML.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ function poemResult(response) {
     strings: response.data.answer,
     autoStart: true,
     cursor: "",
-    delay: 10,
+    delay: 7,
   });
 }
 
@@ -13,9 +13,13 @@ function generatePoem(event) {
   let userSubmission = document.querySelector("#submission");
   let prompt = `write a nice short color poem about ${userSubmission.value}`;
   let context =
-    "You are a savy poet, and your expertise is writing poems about color. Use basic html on the poem text when generating the poem, and separate each line with a <br />. Only style the text in the color black. Sign the poem as '- SheCodes AI'.";
+    "You are a savy poet, and your expertise is writing nice short poems about color. Use basic html on the poem text when generating the poem, and separate each line with a <br />. Only style the text in the color black. Sign the poem as '- SheCodes AI'.";
   let apiKey = "2f3f1baf409eboat1de63d1abc36da6b";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = "Writing you a poem... ✏️";
 
   axios.get(apiUrl).then(poemResult);
 }
